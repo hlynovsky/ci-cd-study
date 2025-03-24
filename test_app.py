63fcaa045bf5b277109da62d774c1c4eb47f9106
Yury <theacer4@gmail.com>
2025-03-24 17:32:59 +0300

    Day 1: Basic CI setup

---
+++ test_app.py
@@ -0,0 +1,8 @@
+from app import app
+import pytest
+
+def test_hello():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Hello, CI/CD!" in response.dat
